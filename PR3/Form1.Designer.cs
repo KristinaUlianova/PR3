4ada3c0fa62d156207757a60459bb11b8136c18f
Kristina <cristina.ulyanowa2016@yandex.ru>
2023-03-09 23:17:57 +0300

    Добавила вторую кнопку

--- PR3/Form1.Designer.cs
+++ PR3/Form1.Designer.cs
@@ -30,6 +30,7 @@ namespace PR3
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.Кнопка2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -42,11 +43,21 @@ namespace PR3
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // Кнопка2
+            // 
+            this.Кнопка2.Location = new System.Drawing.Point(154, 28);
+            this.Кнопка2.Name = "Кнопка2";
+            this.Кнопка2.Size = new System.Drawing.Size(75, 23);
+            this.Кнопка2.TabIndex = 1;
+            this.Кнопка2.Text = "Кнопка2";
+            this.Кнопка2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(300, 105);
+            this.Controls.Add(this.Кнопка2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,6 +68,7 @@ namespace PR3
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Кнопка2;
     }
 }
 
